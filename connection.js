fecf840e3cae28377d10c39a2db8db0d8def18d9
antoniopaolillo <antonio_francisco_p@live.com>
2020-07-15 17:14:00 -0300

    add database

---
+++ connection.js
@@ -0,0 +1,22 @@
+const mongoClient = require('mongodb').MongoClient;
+
+const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+
+const connection = () => {
+  return mongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db('web_chat'))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+};
+
+const getAll = async () => connection().then((db) => db.collection('messages').find().toArray());
+
+const sendMessage = async msg => connection().then((db) => db.collection('messages').insertOne({ message: msg }));
+
+module.exports = { getAll, sendMessage };
--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
+const { getAll, sendMessage } = require('./connection');
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-io.on('connection', (socket) => {
+
+io.on('connection', async (socket) => {
   socket.broadcast.emit('mensagemServer');
-  console.log('Conectado');
+  const messages = await getAll();
+  messages.forEach(({ message }) => socket.emit('mensagemServer', message));
 
   socket.on('disconnect', () => {
     console.log('desconectado')
     io.emit('adeus', { mensagem: 'Poxa, fica mais, vai ter bolo :)' });
   });
-  socket.on('mensagem', (msg) => {
+  socket.on('mensagem', async (msg) => {
+    await sendMessage(msg);
     io.emit('mensagemServer', msg);
   });
 });
