c33a677486a52d35a8a01ef6a6b2effef5bab0b9
antoniopaolillo <antonio_francisco_p@live.com>
2020-07-15 16:59:27 -0300

    start project

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+io.on('connection', (socket) => {
+  socket.broadcast.emit('mensagemServer');
+  console.log('Conectado');
+
+  socket.on('disconnect', () => {
+    console.log('desconectado')
+    io.emit('adeus', { mensagem: 'Poxa, fica mais, vai ter bolo :)' });
+  });
+  socket.on('mensagem', (msg) => {
+    io.emit('mensagemServer', msg);
+  });
+});
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
