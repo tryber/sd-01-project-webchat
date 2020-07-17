e2aec4d08fc33f753f8d972c8cab7ab80993aa1e
DougFunny <douglaseduardo220483@gmail.com>
2020-07-16 23:51:22 -0300

    ESLINT

--- frontend/src/service/formatDate.js
+++ frontend/src/service/formatDate.js
@@ -1,9 +1,9 @@
 export default function formatDate() {
   const data = new Date();
   const dia = data.getDate().toString();
-  const diaF = dia.length == 1 ? '0' + dia : dia;
+  const diaF = dia.length === 1 ? '0' + dia : dia;
   const mes = (data.getMonth() + 1).toString();
-  const mesF = mes.length == 1 ? '0' + mes : mes;
+  const mesF = mes.length === 1 ? '0' + mes : mes;
   const anoF = data.getFullYear();
   const hora = data.getHours();
   const minuto = data.getMinutes();
